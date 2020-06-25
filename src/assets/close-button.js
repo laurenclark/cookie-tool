fc8acc2b096e0809b8f40600cd10f8da2c8f94f0
Lauren Clark <lauren.c4231@gmail.com>
2020-06-25 16:19:49 +0100

    Add SVG's as template string includes

---
+++ src/assets/close-button.js
@@ -0,0 +1,18 @@
+const closeButton = `
+<? xml version = "1.0" encoding = "UTF-8" ?>
+<svg
+    aria-role="button"
+    width="20px"
+    height="20px"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 25 25"
+>
+    <title>Close Icon</title>
+    <polygon
+        class="x"
+        points="25 0.71 24.29 0 12.5 11.79 0.71 0 0 0.71 11.79 12.5 0 24.29 0.71 25 12.5 13.21 24.29 25 25 24.29 13.21 12.5 25 0.71"
+    ></polygon>
+</svg>`;
+
+export default closeButton;
