770c588b3753130bc67959fb3f35d1212ae067d7
Lauren Clark <lauren.c4231@gmail.com>
2020-06-25 17:21:17 +0100

    Update compiled styles

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 /*--------------------------------------------------------------
 ## ⚠️ Uncomment this to build the styles in dist.
 --------------------------------------------------------------*/
-import './styles/main.sass';
+// import './styles/main.sass';
 
 import CookieButton from './components/CookieButton';
 import Dialog from './components/Dialog';
 import InfoDialog from './components/InfoDialog';
-// import compiledStyles from './compiledStyles';
+import compiledStyles from './compiledStyles';
 
 import App from './app';
 
@@ -18,9 +18,9 @@ template.innerHTML = `
         ${InfoDialog()}
     `;
 
-// const styles = document.createElement('style');
-// styles.innerHTML = compiledStyles;
+const styles = document.createElement('style');
+styles.innerHTML = compiledStyles;
 
-// document.head.appendChild(styles);
+document.head.appendChild(styles);
 document.body.insertBefore(template, document.body.firstChild);
 App();
