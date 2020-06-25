97dcf965a5b303ad388a5f7ecc9fdd93a4daf69d
Lauren Clark <lauren.c4231@gmail.com>
2020-06-25 14:08:59 +0100

    Inject styles into the head

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import './styles/main.sass';
+/*--------------------------------------------------------------
+## ⚠️ Uncomment this to build the styles in dist.
+--------------------------------------------------------------*/
+// import './styles/main.sass';
+
 import CookieButton from './components/CookieButton';
 import Dialog from './components/Dialog';
 import InfoDialog from './components/InfoDialog';
+import compiledStyles from './compiledStyles';
+
 import App from './app';
 
 const template = document.createElement('div');
@@ -12,5 +18,9 @@ template.innerHTML = `
         ${InfoDialog()}
     `;
 
+const styles = document.createElement('style');
+styles.innerHTML = compiledStyles;
+
+document.head.appendChild(styles);
 document.body.insertBefore(template, document.body.firstChild);
 App();
