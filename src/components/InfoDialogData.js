be92da2b4befc198aa87988294bffadd90f55906
Lauren Clark <lauren.c4231@gmail.com>
2020-06-25 13:21:06 +0100

    Add InfoDialogData for rendering out into InfoDialog template

---
+++ src/components/InfoDialogData.js
@@ -0,0 +1,29 @@
+const InfoDialogData = [
+    {
+        marketing: {
+            title: 'Marketing',
+            text:
+                'Used to deliver advertising that is more relevant to you and your interests. They are also used to limit the number of times you see an advertisement as well as help measure the effectiveness of the advertising campaign. They are usually placed by advertising networks with the website operator’s permission.',
+            scripts: [],
+            cookies: [],
+        },
+    },
+    {
+        personalisation: {
+            title: 'Personalisation',
+            text:
+                'Allows the website to remember choices you make (such as your user name, language or the region you are in) and provide enhanced, more personal features. For example, a website may be able to provide you with local weather reports or traffic news by storing data about the region in which you are currently located.',
+            scripts: [],
+            cookies: [],
+        },
+    },
+    {
+        analytics: {
+            title: 'Analytics',
+            text:
+                'Provides information about how a web site performs, how each page renders, and whether there are technical issues on the web site to the web site operator. This storage type generally doesn’t collect information that identifies a visitor.',
+            scripts: [],
+            cookies: [],
+        },
+    },
+];
