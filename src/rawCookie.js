d8876efc18bc5c45394de2ff7fad7469d3f38202
Lauren Clark <lauren.c4231@gmail.com>
2020-06-30 16:50:53 +0100

    Only run sass styles on dev and don't inject them.

--- src/rawCookie.js
+++ src/rawCookie.js
@@ -1,10 +1,3 @@
-/*--------------------------------------------------------------
-## ⚠️ Uncomment this to build the styles in dist.
-## Comment it out for development if not editing styles.
-## TODO - Put this in some environment conditional.
---------------------------------------------------------------*/
-import './styles/main.sass';
-
 import { State as state } from './config/State';
 import { config } from './config/Config';
 
@@ -16,5 +9,7 @@ window.addEventListener('DOMContentLoaded', function onLoad() {
     // Init
     InjectTemplate(config);
     App(state, config);
-    // InjectStyles();
+    if (process.env.NODE_ENV === 'production') {
+        InjectStyles();
+    }
 });
