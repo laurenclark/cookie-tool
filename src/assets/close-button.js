2ce4f6225e71fda929499d10b04d4db549457d9b
Lauren Clark <lauren.c4231@gmail.com>
2020-07-01 16:59:18 +0100

    💄Change close button and style

--- src/assets/close-button.js
+++ src/assets/close-button.js
@@ -1,18 +1,13 @@
 const closeButton = `
-<? xml version = "1.0" encoding = "UTF-8" ?>
-<svg
-    aria-role="button"
-    width="20px"
-    height="20px"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    viewBox="0 0 25 25"
->
-    <title>Close Icon</title>
-    <polygon
-        class="x"
-        points="25 0.71 24.29 0 12.5 11.79 0.71 0 0 0.71 11.79 12.5 0 24.29 0.71 25 12.5 13.21 24.29 25 25 24.29 13.21 12.5 25 0.71"
-    ></polygon>
-</svg>`;
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="100%" height="100%" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(0.0560224,0,0,0.0560224,0,0)">
+        <g id="close">
+            <path d="M357,35.7L321.3,0L178.5,142.8L35.7,0L0,35.7L142.8,178.5L0,321.3L35.7,357L178.5,214.2L321.3,357L357,321.3L214.2,178.5L357,35.7Z" style="fill:white;fill-rule:nonzero;"/>
+        </g>
+    </g>
+</svg>
+`;
 
 export default closeButton;
