caee776deef1aebc1c4f0fa1b98291222ff18fe3
Lauren Clark <lauren.c4231@gmail.com>
2020-07-07 14:24:19 +0100

    💄 Remove fill from SVG and add it as FG color

--- src/assets/close-button.js
+++ src/assets/close-button.js
@@ -4,10 +4,10 @@ const closeButton = `
 <svg width="100%" height="100%" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
     <g transform="matrix(0.0560224,0,0,0.0560224,0,0)">
         <g id="close">
-            <path d="M357,35.7L321.3,0L178.5,142.8L35.7,0L0,35.7L142.8,178.5L0,321.3L35.7,357L178.5,214.2L321.3,357L357,321.3L214.2,178.5L357,35.7Z" style="fill:white;fill-rule:nonzero;"/>
+            <path d="M357,35.7L321.3,0L178.5,142.8L35.7,0L0,35.7L142.8,178.5L0,321.3L35.7,357L178.5,214.2L321.3,357L357,321.3L214.2,178.5L357,35.7Z"/>
         </g>
     </g>
 </svg>
-`;
+`
 
-export default closeButton;
+export default closeButton
