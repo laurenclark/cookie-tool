3cbcce43f4584245ad460c7f71ffec7ddefbd44f
Lauren Clark <lauren.c4231@gmail.com>
2020-07-22 12:29:22 +0100

    Add currentScript polyfill

--- src/modules/CustomColors.js
+++ src/modules/CustomColors.js
@@ -1,23 +1,25 @@
+require('../lib/currentScript-polyfill')
+
 /*--------------------------------------------------------------
 ## Get Color Datasets
 --------------------------------------------------------------*/
-const scriptTag = document.currentScript;
+const scriptTag = document.currentScript
 
 export const dialogBG =
-    scriptTag.dataset.colorPrimarybg && scriptTag.dataset.colorPrimarybg;
+    scriptTag.dataset.colorPrimarybg && scriptTag.dataset.colorPrimarybg
 
 export const dialogFG =
-    scriptTag.dataset.colorPrimaryfg && scriptTag.dataset.colorPrimaryfg;
+    scriptTag.dataset.colorPrimaryfg && scriptTag.dataset.colorPrimaryfg
 
 export const infoBG =
-    scriptTag.dataset.colorSecondarybg && scriptTag.dataset.colorSecondarybg;
+    scriptTag.dataset.colorSecondarybg && scriptTag.dataset.colorSecondarybg
 
 export const infoFG =
-    scriptTag.dataset.colorSecondaryfg && scriptTag.dataset.colorSecondaryfg;
+    scriptTag.dataset.colorSecondaryfg && scriptTag.dataset.colorSecondaryfg
 
-export const link = scriptTag.dataset.colorLink && scriptTag.dataset.colorLink;
+export const link = scriptTag.dataset.colorLink && scriptTag.dataset.colorLink
 
 export const buttonBG =
-    scriptTag.dataset.colorButtonbg && scriptTag.dataset.colorButtonbg;
+    scriptTag.dataset.colorButtonbg && scriptTag.dataset.colorButtonbg
 export const buttonFG =
-    scriptTag.dataset.colorButtonfg && scriptTag.dataset.colorButtonfg;
+    scriptTag.dataset.colorButtonfg && scriptTag.dataset.colorButtonfg
